85c2c5d25d1bceed94e2b18a0d4f69485de3b629
keerthanamoorthy9687 <102482438+keerthanamoorthy9687@users.noreply.github.com>
2025-02-15 20:08:25 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Content from './Component/Content';
+// import Register from './Component/Register';
+import Login from './Component/Login';
+// import ProductDetails from './Component/Product Details';
+
+const App  = () => {
+  return (
+    <> 
+     {/* <div className="contents"><ProductDetails/></div> */}
+     {/* <div className="contents"><Content/></div> */}
+     <div className="login"><Login/></div>
+      
+    </>
+
+   
+  )
+}
+export default App;
+
+    
